fd4178f754eac109bf856c967c15db7cacf68b01
Robert Stiff <uatecuk@gmail.com>
2025-12-20 23:24:23 +0000

    ping support

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,15 +1,21 @@
 import AppKit
+import Foundation
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     var statusItem: NSStatusItem!
+    var timer: Timer?
+    var recentPings: [Double] = []
+    let windowSize = 5
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         // Create the status item
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         
-        // Setup the button image
+        // Setup the button
         if let button = statusItem.button {
-            button.image = NSImage(systemSymbolName: "globe", accessibilityDescription: "Hello World App")
+            button.image = NSImage(systemSymbolName: "globe", accessibilityDescription: "Ping Meter")
+            button.title = " -- ms"
+            button.imagePosition = .imageLeft
         }
         
         // Create the menu
@@ -30,14 +36,74 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         // Assign menu to status item
         statusItem.menu = menu
+        
+        // Start pinging
+        startPingTimer()
+    }
+    
+    func startPingTimer() {
+        // Fire every 1 second
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
+            self?.performPing()
+        }
+    }
+    
+    func performPing() {
+        DispatchQueue.global(qos: .background).async {
+            let task = Process()
+            task.launchPath = "/sbin/ping"
+            task.arguments = ["-c", "1", "1.1.1.1"]
+            
+            let pipe = Pipe()
+            task.standardOutput = pipe
+            
+            do {
+                try task.run()
+                let data = pipe.fileHandleForReading.readDataToEndOfFile()
+                if let output = String(data: data, encoding: .utf8) {
+                    self.parsePingOutput(output)
+                }
+            } catch {
+                print("Failed to run ping: \(error)")
+            }
+        }
+    }
+    
+    func parsePingOutput(_ output: String) {
+        // Example output: ... time=14.5 ms ...
+        // We look for "time=" and then the number
+        
+        let components = output.components(separatedBy: "time=")
+        if components.count > 1 {
+            let valuePart = components[1]
+            // Extract the number (take characters until space)
+            let numberString = valuePart.components(separatedBy: " ")[0]
+            
+            if let timeMs = Double(numberString) {
+                DispatchQueue.main.async {
+                    self.updateAverage(timeMs)
+                }
+            }
+        }
+    }
+    
+    func updateAverage(_ newTime: Double) {
+        recentPings.append(newTime)
+        if recentPings.count > windowSize {
+            recentPings.removeFirst()
+        }
+        
+        let average = recentPings.reduce(0, +) / Double(recentPings.count)
+        
+        if let button = statusItem.button {
+            button.title = String(format: " %.1f ms", average)
+        }
     }
     
     @objc func helloWorldClicked() {
-        print("Hello World clicked")
-        // Optionally show an alert
         let alert = NSAlert()
         alert.messageText = "Hello World"
-        alert.informativeText = "You clicked the menu item!"
+        alert.informativeText = "Ping Meter is running!"
         alert.alertStyle = .informational
         alert.addButton(withTitle: "OK")
         alert.runModal()
@@ -52,7 +118,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 let app = NSApplication.shared
 let delegate = AppDelegate()
 app.delegate = delegate
-app.setActivationPolicy(.accessory) // Hide from Dock, show only in menu bar
+app.setActivationPolicy(.accessory)
 
 // Run the app
 app.run()
